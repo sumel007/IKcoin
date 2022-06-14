dd4ba476eac144d7849dc972c27f40b256e52f1a
sumel007 <ehsan15-536@diu.edu.bd>
2022-06-15 00:42:55 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'primary-color':'#ffc400',
+        'deepblack' : '#202020'
+        
+      }
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+   // require('@tailwindcss/forms'),
+  ],
+}
+
+
